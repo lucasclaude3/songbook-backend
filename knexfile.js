871eebe3599a3fce4db562c2c1602f7f67b85e98
kahlua <lucasclaude3@gmail.com>
2018-10-17 20:52:54 +0200

    Add knex and create users table

---
+++ knexfile.js
@@ -0,0 +1,9 @@
+module.exports = {
+    client: 'mysql',
+    connection: {
+        host : 'localhost',
+        user : 'root',
+        password : 'superS0hcaht0@',
+        database : 'songbook'
+    }
+}
