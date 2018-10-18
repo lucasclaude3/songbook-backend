52d59fe1fa7a495ae57b95db0604b3786c586fed
kahlua <lucasclaude3@gmail.com>
2018-10-18 16:01:51 +0200

    Add a route to create users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex')(require('./../knexfile'));
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/create', function(req, res, next) {
+  knex('users')
+    .insert({
+      username: req.body.username,
+      password: req.body.password,
+    })
+    .then((result) => res.send('create a resource'));
+})
 
 module.exports = router;
